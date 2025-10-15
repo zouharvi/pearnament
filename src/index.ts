943e5e3d54289df3425897a2765e3f665ba2356d
Vilém Zouhar <vilem.zouhar@gmail.com>
2025-10-15 16:20:08 +0200

    add mock data and icon

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,72 @@
 import $ from 'jquery';
+import { get_next_pair } from './connector';
 
-$(document).ready(() => {
-  console.log('jQuery is working!');
-  
-  // Create a simple demo element
-  // const $container = $('<div>')
-  //   .attr('id', 'app')
-  //   .html('<h1>Welcome to Pearnament</h1><p>TypeScript + Webpack + jQuery setup is working!</p>')
-  //   .css({
-  //     'text-align': 'center',
-  //     'padding': '50px',
-  //     'font-family': 'Arial, sans-serif'
-  //   });
-  
-  // $('body').append($container);
-  
-  // // Add a simple click handler demo
-  // const $button = $('<button>')
-  //   .text('Click me!')
-  //   .css({
-  //     'padding': '10px 20px',
-  //     'font-size': '16px',
-  //     'cursor': 'pointer',
-  //     'margin-top': '20px'
-  //   })
-  //   .on('click', () => {
-  //     alert('Button clicked! jQuery event handler is working.');
-  //   });
-  
-  // $container.append($button);
+$("#button_left").on("click", function() {
+  $(".button_navigation").removeClass("button_selected")
+  $(this).addClass("button_selected")
+  $("#button_next").removeAttr("disabled")
+})
+$("#button_right").on("click", function() {
+  $(".button_navigation").removeClass("button_selected")
+  $(this).addClass("button_selected")
+  $("#button_next").removeAttr("disabled")
+})
+$("#button_bothgood").on("click", function() {
+  $(".button_navigation").removeClass("button_selected")
+  $(this).addClass("button_selected")
+  $("#button_next").removeAttr("disabled")
+})
+$("#button_bothbad").on("click", function() {
+  $(".button_navigation").removeClass("button_selected")
+  $(this).addClass("button_selected")
+  $("#button_next").removeAttr("disabled")
+})
+
+$(document).on('keypress', function (e) {
+  if (e.key === 'e' || e.key === 'E')
+    $("#button_left").trigger("click")
+  if (e.key === 'r' || e.key === 'R')
+    $("#button_right").trigger("click")
+  if (e.key === 'd' || e.key === 'D')
+    $("#button_bothbad").trigger("click")
+  if (e.key === 'f' || e.key === 'F')
+    $("#button_bothgood").trigger("click")
+  if (e.key === 'n' || e.key === 'N')
+    $("#button_next").trigger("click")
 });
+
+$("#toggle_differences").on("change", function() {
+  if ($(this).is(":checked")) {
+    $(".difference").removeClass("hidden")
+  } else {
+    $(".difference").addClass("hidden")
+  }
+})
+
+
+function load_next() {
+  let data = get_next_pair()
+
+  let html_new = ""
+  for (let i = 0; i < data.src.length; i++) {
+    html_new += `
+    <div class="output">${data.out_a[i]}</div>
+    <div class="output">${data.src[i]}</div>
+    <div class="output">${data.out_b[i]}</div>
+    <br>
+    <br>
+    `
+  }
+  console.log(html_new)
+  $("#output_div").html(html_new)
+
+  console.log($("#output_div"))
+}
+
+$("#button_next").attr("disabled", "disabled")
+
+$("#button_next").on("click", function() {
+  load_next()
+})
+
+load_next()
