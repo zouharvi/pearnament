2e8302e8842ccc3b43f6ca3ddf410d0d470bd1db
Vilém Zouhar <vilem.zouhar@gmail.com>
2025-10-15 16:20:08 +0200

    add mock data and icon

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/index.ts',
@@ -24,6 +25,11 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './src/index.html',
       inject: 'body',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'src/assets', to: 'assets' },
+      ],
     }),
   ],
   devServer: {
